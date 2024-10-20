481b29916d2276d33c6cddaca317b0d88b77bc0c
Corina Simionescu <corina.simionescu12@gmail.com>
2024-10-20 21:15:38 +0300

    Configure vite.config.js file for production

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,22 +1,24 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   build: {
+    outDir: "dist",
+    emptyOutDir: true,
     manifest: true,
     rollupOptions: {
-      input: '/src/main.jsx',
+      input: "/src/main.jsx",
     },
   },
   server: {
     proxy: {
-      '/api': {
-        target: 'http://localhost:3000',
+      "/api": {
+        target: "http://localhost:3000",
         changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, '')
-      }
-    }
-  }
-})
+        rewrite: (path) => path.replace(/^\/api/, ""),
+      },
+    },
+  },
+});
